f7ca04638eef1538c05332c6d931d1cdd752a9c8
Architector <katusha2105@gmail.com>
2026-04-03 13:10:32 +0300

    second

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
+from typing import Union
+
+
 class Distance:
     def __init__(self, km: float) -> None:
-        self.km = km
+        self.km: float = km
 
     def __str__(self) -> str:
         return f"Distance: {self.km} kilometers."
@@ -8,40 +11,40 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other):
+    def __add__(self, other: Union["Distance", float]) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Union["Distance", float]) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
             self.km += other
         return self
 
-    def __mul__(self, other):
+    def __mul__(self, other: float) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: float) -> "Distance":
         return Distance(round(self.km / other, 2))
 
-    def _get_value(self, other):
+    def _get_value(self, other: Union["Distance", float]) -> float:
         if isinstance(other, Distance):
             return other.km
         return other
 
-    def __lt__(self, other):
+    def __lt__(self, other: Union["Distance", float]) -> bool:
         return self.km < self._get_value(other)
 
-    def __gt__(self, other):
+    def __gt__(self, other: Union["Distance", float]) -> bool:
         return self.km > self._get_value(other)
 
-    def __eq__(self, other):
+    def __eq__(self, other: Union["Distance", float]) -> bool:
         return self.km == self._get_value(other)
 
-    def __le__(self, other):
+    def __le__(self, other: Union["Distance", float]) -> bool:
         return self.km <= self._get_value(other)
 
-    def __ge__(self, other):
+    def __ge__(self, other: Union["Distance", float]) -> bool:
         return self.km >= self._get_value(other)
